311018a50e66802f36b1ed730aa0e56b789b723e
hyoungjin <cent2100@naver.com>
2024-04-08 00:05:18 +0900

    init

---
+++ src/main/java/com/example/ktdsOCS/KtdsOcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktdsOCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtdsOcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtdsOcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ktdsOCS/KtdsOcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktdsOCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtdsOcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
